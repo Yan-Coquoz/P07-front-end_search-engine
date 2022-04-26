7870be59451fa1d1d5e1b2373ff75d919e6293d9
Yan Coquoz <Yan.Coquoz@gmail.com>
2022-04-26 17:43:34 +0200

    Petit ajustement sur les cartes

--- src/models/CardRecipes.js
+++ src/models/CardRecipes.js
@@ -26,7 +26,7 @@ export class CardRecipes {
     const desc = document.createElement("p");
 
     article.classList.add("card", "g-4");
-    article.style.width = "18rem";
+    article.style.width = "22rem";
 
     upperContainerImg.classList.add("card-img-top");
     upperContainerImg.setAttribute("src", "./public/assets/imgRecette.svg");
@@ -39,7 +39,9 @@ export class CardRecipes {
       const baseObject = this.ingredients[props];
       const ingredient = baseObject.ingredient;
       const li = document.createElement("li");
+      const strong = document.createElement("strong");
       let quantity, unit;
+
       if (baseObject.quantite || baseObject.quantity) {
         quantity = baseObject.quantity
           ? baseObject.quantity
@@ -48,22 +50,38 @@ export class CardRecipes {
       if (baseObject.unit) {
         unit = baseObject.unit;
       }
-      const contentProps = ` ${ingredient} : ${quantity ? quantity : ""} ${
+      strong.classList.add("list_ingredient");
+      strong.textContent = `${ingredient} `;
+
+      const contentProps = ` ${quantity ? `: ${quantity}` : ""} ${
         unit ? unit : ""
       }`;
 
       li.textContent = contentProps;
+      li.insertAdjacentElement("afterbegin", strong);
       ingredientBox.appendChild(li);
     }
+    descBloc.classList.add(
+      "d-flex",
+      "flex-row",
+      "justify-content-between",
+      "gap-1"
+    );
+    ingredientBox.classList.add("col-5", "mb-0");
+    desc.classList.add("col-7", "m-0");
+
+    icon.setAttribute("src", "../../public/assets/clock.svg");
+    icon.setAttribute("alt", "icone d'horloge");
+    icon.classList.add("p-0");
 
     title.classList.add("card-title");
     title.textContent = this.name;
 
-    time.textContent = this.time;
+    time.textContent = ` ${this.time} min`;
     desc.textContent = this.description;
 
-    timeBloc.appendChild(time);
     timeBloc.appendChild(icon);
+    timeBloc.appendChild(time);
     title.appendChild(timeBloc);
     descBloc.appendChild(ingredientBox);
     descBloc.appendChild(desc);
--- src/models/SearchByTag.js
+++ src/models/SearchByTag.js
@@ -1,4 +1,8 @@
 export class SearchByTag {
+  constructor() {
+    this.icon = "../../public/assets/dropdown.svg";
+    this.selected = this.selected.bind(this);
+  }
   /**
    * @returns {HTMLElement}
    */
@@ -6,21 +10,44 @@ export class SearchByTag {
     const container = document.querySelector("#searching_bar");
     const div = document.createElement("div");
     div.innerHTML = `<!-- ingredient -->
-                    <div class="sort__blue py-2">
-                        <input name="ingredient" class="form-control bg-primary text-light" id="ingredient" placeholder="Ingrédients">
+                    <div class="sort__blue input-group col-4 py-2">
+                        <input name="ingredient" class="form-control bg-primary " id="ingredient" placeholder="Ingrédients">
+                        <img src="${this.icon}" alt="icone tête de flèche">
 
                     </div>
                     <!-- appareils -->
-                    <div class="sort__green  py-2">
-                        <input name="appareils" class="form-control bg-success text-light" id="appareils" placeholder="appareils">
-
+                    <div class="sort__green input-group col-4 py-2">
+                        <input name="appareils" class="form-control bg-success " id="appareil" placeholder="Appareils">
+                        <img src="${this.icon}" alt="icone tête de flèche">
                     </div>
                     <!-- ustensiles -->
-                    <div class="sort__red  py-2">
-                        <input name="ustensiles" class="form-control bg-danger text-light " id="ustensiles" placeholder="ustensiles">
+                    <div class="sort__red input-group col-4 py-2">
+                        <input name="ustensiles" class="form-control bg-danger  " id="ustensile" placeholder="Ustensiles">
+                        <img src="${this.icon}" alt="icone tête de flèche">
                     </div>`;
+
+    div.querySelectorAll("input").forEach((inputElement) => {
+      inputElement.addEventListener("input", this.selected);
+    });
 
     div.classList.add("sort", "col-12", "col-sm-4", "form-group");
     return container.appendChild(div);
+  }
+  /**
+   * @param {KeyboardEvent} evt
+   */
+  selected(evt) {
+    switch (evt.target.id) {
+      case "ingredient":
+        console.log("ingredient", evt.target.value);
+        break;
+      case "appareil":
+        console.log("appareil", evt.target.value);
+        break;
+      case "ustensile":
+        console.log("ustensile", evt.target.value);
+        break;
+    }
+    document.removeEventListener("input", this.selected);
   }
 }
