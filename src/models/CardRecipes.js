2ac20ebca9a9c66c21c5fd1edf6d895d126f406f
Yan Coquoz <Yan.Coquoz@gmail.com>
2022-04-27 16:20:44 +0200

    Mise en place de la carte

--- src/models/CardRecipes.js
+++ src/models/CardRecipes.js
@@ -17,6 +17,7 @@ export class CardRecipes {
     const article = document.createElement("article");
     const upperContainerImg = document.createElement("img");
     const lowerContainer = document.createElement("div");
+    const titleBox = document.createElement("div");
     const timeBloc = document.createElement("div");
     const title = document.createElement("h2");
     const time = document.createElement("span");
@@ -25,13 +26,10 @@ export class CardRecipes {
     const ingredientBox = document.createElement("ul");
     const desc = document.createElement("p");
 
-    article.classList.add("card", "g-4");
-    article.style.width = "22rem";
-
-    upperContainerImg.classList.add("card-img-top");
     upperContainerImg.setAttribute("src", "./public/assets/imgRecette.svg");
     upperContainerImg.setAttribute("alt", "Photo de la recette ");
 
+    upperContainerImg.classList.add("card-img-top");
     lowerContainer.classList.add("card-body");
 
     // rendu des ingredients
@@ -39,8 +37,9 @@ export class CardRecipes {
       const baseObject = this.ingredients[props];
       const ingredient = baseObject.ingredient;
       const li = document.createElement("li");
+      li.classList.add("list-inline-none");
       const strong = document.createElement("strong");
-      let quantity, unit;
+      let quantity, unit, quantityString;
 
       if (baseObject.quantite || baseObject.quantity) {
         quantity = baseObject.quantity
@@ -52,8 +51,8 @@ export class CardRecipes {
       }
       strong.classList.add("list_ingredient");
       strong.textContent = `${ingredient} `;
-
-      const contentProps = ` ${quantity ? `: ${quantity}` : ""} ${
+      quantityString = `: ${quantity}`;
+      const contentProps = ` ${quantity ? quantityString : ""} ${
         unit ? unit : ""
       }`;
 
@@ -61,32 +60,52 @@ export class CardRecipes {
       li.insertAdjacentElement("afterbegin", strong);
       ingredientBox.appendChild(li);
     }
+    icon.setAttribute("src", "./public/assets/clock.svg");
+    icon.setAttribute("alt", "icone d'horloge");
+
+    //classe Bootstrap
+    article.classList.add("card", "g-4", "overflow-hidden");
     descBloc.classList.add(
+      "desc_bloc",
       "d-flex",
       "flex-row",
       "justify-content-between",
-      "gap-1"
+      "overflow-hidden",
+      "my-2"
     );
-    ingredientBox.classList.add("col-5", "mb-0");
-    desc.classList.add("col-7", "m-0");
 
-    icon.setAttribute("src", "./public/assets/clock.svg");
-    icon.setAttribute("alt", "icone d'horloge");
+    ingredientBox.classList.add("col-5", "mb-0", "list-inline", "w-50");
+    desc.classList.add("col-5", "m-0");
     icon.classList.add("p-0");
+    titleBox.classList.add("d-flex", "justify-content-between", "fw-bold");
+    timeBloc.classList.add("w-50", "text-end");
+    title.classList.add("card-title", "fw-bold");
 
-    title.classList.add("card-title");
     title.textContent = this.name;
-
     time.textContent = ` ${this.time} min`;
-    desc.textContent = this.description;
+
+    if (this.description.length >= 165) {
+      console.log("je suis au dessus de 172");
+
+      const text = this.description.slice(0, 150) + "...";
+      console.log(text.length);
+      desc.textContent = text;
+    } else {
+      desc.textContent = this.description;
+    }
 
     timeBloc.appendChild(icon);
     timeBloc.appendChild(time);
-    title.appendChild(timeBloc);
+
+    titleBox.appendChild(title);
+    titleBox.appendChild(timeBloc);
+
     descBloc.appendChild(ingredientBox);
     descBloc.appendChild(desc);
-    lowerContainer.appendChild(title);
+
+    lowerContainer.appendChild(titleBox);
     lowerContainer.appendChild(descBloc);
+
     article.appendChild(upperContainerImg);
     article.appendChild(lowerContainer);
 
